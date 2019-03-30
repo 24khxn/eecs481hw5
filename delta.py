b44dbfdffdf32873a05f02a867683d2f1e622959
jchinnam <jchinnam@umich.edu>
2019-03-30 17:58:38 -0400

    read inputs

---
+++ delta.py
@@ -0,0 +1,12 @@
+import os
+import subprocess
+import sys
+
+def main():
+    if len(sys.argv) != 3: exit(1)
+
+    set_size = sys.argv[1] # n, size of set to be minimized
+    interesting_check = sys.argv[2] # command to check if set is interesting
+
+if __name__ == "__main__":
+    main()
