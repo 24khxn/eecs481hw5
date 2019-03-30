ec8fb784654baa0594e9393af9ec9252da6c8b5f
jchinnam <jchinnam@umich.edu>
2019-03-30 18:45:41 -0400

    small type fixes

--- delta.py
+++ delta.py
@@ -9,27 +9,28 @@ set_size = int(sys.argv[1]) # n, size of set to be minimized
 interesting_check = sys.argv[2] # command to check if set is interesting
 
 def main():
-    set_range = list(range(set_size))
+    set_range = list(range(set_size)) # range of changes {c_1, c_2, ..., c_n}
 
-    min_set = minimize(set(), set_range)
-    return min_set.sort()
+    p = minimize(set(), set_range)
+    return p.sort()
 
-def minimize(min_set, set_range):
+# delta debugging algorithm, see slides for pseudocode
+def minimize(p, set_range):
 
     # base case
     if len(set_range) == 1:
         return set_range
 
-    p1, p2 = split_list(set_range)
+    p1, p2 = split(set_range)
 
-    if is_interesting(min_set.union(p1)):
-        return minimize(min_set, p1)
-    if is_interesting(min_set.union(p2)):
-        return minimize(min_set, p2)
+    if is_interesting(p.union(p1)):
+        return minimize(p, p1)
+    if is_interesting(p.union(p2)):
+        return minimize(p, p2)
 
-    return minimize(min_set.union(p2), p1).union(minimize(min_set.union(p1), p2))
+    return minimize(p.union(p2), p1).union(minimize(p.union(p1), p2))
 
-def split_list(set_range):
+def split(set_range):
     half = len(set_range)//2
     return set_range[:half], set_range[half:]
 
